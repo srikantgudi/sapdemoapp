5cf4968234a8a828535d55b59d3f815b9d53d610
Srikant Gudi <srikantgudi@gmail.com>
2020-11-28 17:10:59 +0530

    First commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,60 @@
+<script>
+	export let segment;
+</script>
+
+<style>
+	nav {
+		display: grid;
+		grid-template-columns: 1fr 2fr;
+		border-bottom: 1px solid rgba(255,62,0,0.1);
+		font-weight: 300;
+		padding: 0 1em;
+	}
+
+	ul {
+		margin: 0;
+		padding: 0;
+	}
+
+	/* clearfix */
+	ul::after {
+		content: '';
+		display: block;
+		clear: both;
+	}
+
+	li {
+		display: block;
+		float: left;
+	}
+
+	[aria-current] {
+		position: relative;
+		display: inline-block;
+	}
+
+	[aria-current]::after {
+		position: absolute;
+		content: '';
+		width: calc(100% - 1em);
+		height: 4px;
+		background-color: rgb(255,62,0);
+		display: block;
+		bottom: 0.5em;
+	}
+
+	a {
+		text-decoration: none;
+		padding: 1em 0.5em;
+		display: block;
+	}
+</style>
+
+<nav>
+	<h2>Demo Apps</h2>
+	<ul>
+		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
+		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
+		<li><a aria-current="{segment === 'clocks' ? 'page' : undefined}" href="clocks">clocks</a></li>
+	</ul>
+</nav>
---
+++ src/routes/index.svelte
@@ -0,0 +1,39 @@
+<script>
+	import successkid from 'images/successkid.jpg';
+</script>
+
+<style>
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+		border-radius: 8px;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+
+<figure>
+	<h2>Have fun with Sapper!!</h2>
+	<img alt="Success Kid" src="{successkid}">
+	<figcaption></figcaption>
+</figure>
+
